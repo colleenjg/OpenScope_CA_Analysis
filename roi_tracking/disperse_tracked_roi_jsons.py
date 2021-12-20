e30dcec3698e23824531255e478f492a6ad46e7f
colleenjg <colleenjgillon@hotmail.com>
2021-12-20 13:26:41 -0500

    Minor README update Updated dispersion code.

--- roi_tracking/disperse_tracked_roi_jsons.py
+++ roi_tracking/disperse_tracked_roi_jsons.py
@@ -5,92 +5,93 @@ from pathlib import Path
 
 
 #############################################
-def get_target_direcs(tracking_file_sources, data_direc):
+def get_target_path(tracking_file_source, target_dir):
     """
-    get_target_direcs(tracking_file_sources, data_direc)
+    get_target_path(tracking_file_source, target_dir)
 
-    Returns the target directories (mouse/session specific) for each json.
+    Returns the target path (mouse/session specific) for a source json.
 
     Required args:
-        - tracking_file_sources (list): source json paths
+        - tracking_file_source (Path): source json path
+        - target_dir (Path)          : main target data directory
+                                       (up to, but excluding mouse directories)
 
     Returns:
-        - datadir (Path): target data directory
+        - tracking_file_target (Path): target json path
     """
     
-    # get file names
-    tracking_files_local = [
-        tracking_file.name for tracking_file in tracking_file_sources
-    ]
-    
     # identify target directories
-    file_name_tokens = [str(tracking_file_local).split("_") 
-        for tracking_file_local in tracking_files_local]
-    mouse_ids = [tokens[tokens.index("mouse") + 1] 
-        for tokens in file_name_tokens]
-    sess_ids = [tokens[tokens.index("session") + 1] 
-        for tokens in file_name_tokens]
+    source_file_name = Path(tracking_file_source).name
+
+    file_name_tokens = str(source_file_name).split("_") 
+    mouse_id = file_name_tokens[file_name_tokens.index("mouse") + 1] 
+    sess_id = file_name_tokens[file_name_tokens.index("session") + 1]
     
-    target_direcs = [
-        Path(
-            glob.glob(str(Path(
-                data_direc, 
-                f"mouse_{mouse_ids[i]}", 
-                f"ophys_session_{sess_ids[i]}", 
-                f"ophys_experiment_*", 
-                "processed"
-                )))[0]
-            ) for i in range(len(file_name_tokens))
-        ]
+    target_direc_pattern = Path(
+        target_dir, 
+        f"mouse_{mouse_id}", 
+        f"ophys_session_{sess_id}", 
+        f"ophys_experiment_*", 
+        "processed"
+        )
+
+    target_direc = glob.glob(str(target_direc_pattern))
     
-    # identify target paths
-    tracking_file_targets = [
-        target_direc.joinpath(tracking_file_local) 
-        for (target_direc, tracking_file_local) in 
-        zip(target_direcs, tracking_files_local)
-    ]
+    if len(target_direc) == 0:
+        raise OSError(
+            f"No directory found with pattern: {target_direc_pattern}."
+            )
+    elif len(target_direc) > 1:
+        target_matches = ", ".join([str(match) for match in target_direc])
+        raise NotImplementedError(
+            f"Multiple directories found with pattern: {target_direc_pattern}, "
+            f"namely {target_matches}"
+            )
+    else:
+        target_direc = target_direc[0]
+
+    # set target path
+    tracking_file_target = Path(target_direc, source_file_name)
     
-    return tracking_file_targets
+    return tracking_file_target
 
 
 #############################################
-def disperse_jsons(datadir, verbose=False):
+def disperse_jsons(target_dir, verbose=False):
     """
-    disperse_jsons(datadir)
+    disperse_jsons(target_dir)
 
-    Disperses jsons in the data directories.
+    Disperses jsons in the target data directories.
 
     Required args:
-        - datadir (Path): target data directory
+        - target_dir (Path): main target data directory
+                             (up to, but excluding mouse directories)
 
     Optional args:
         - verbose (bool): if True, copying logs are printed to the console
                           default: False
     """
 
-    datadir = Path(datadir)
-    if not datadir.exists():
-        raise OSError(f"{datadir} does not exist.")
+    target_dir = Path(target_dir)
+    if not target_dir.is_dir():
+        raise OSError(f"{target_dir} is not a directory.")
 
     tracking_files_direc = Path("nway-matched-dfs")
-    if not tracking_files_direc.exists():
-        raise OSError(f"{tracking_files_direc} directory is missing.")
+    if not tracking_files_direc.is_dir():
+        raise OSError(f"{tracking_files_direc} is not a directory.")
 
     tracking_file_sources = sorted([Path(file_direc) 
         for file_direc in glob.glob(str(Path(tracking_files_direc, "*.json")))
     ])
-    tracking_file_targets = get_target_direcs(tracking_file_sources, datadir)
 
     # copy files over
-    for (file_source, file_target) in zip(
-        tracking_file_sources, tracking_file_targets
-        ):
-        
+    for file_source in tracking_file_sources:
+        file_target = get_target_path(file_source, target_dir)
         shutil.copy(file_source, file_target)
         if verbose:
+            source_file = file_source.name
             target_direc = file_target.parent
-            target_file = file_target.name
-            print(f"Copied {target_file} to {target_direc}.")
+            print(f"Copied {source_file} to {target_direc}.")
 
 
 #############################################
@@ -99,13 +100,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
         # data parameters
-    parser.add_argument("--datadir", type=Path,
-        help="target data directory (up to, but excluding mouse directories)")
+    parser.add_argument("--target_dir", type=Path,
+        help=("main target data directory (up to, but excluding mouse "
+            "directories)"))
     parser.add_argument("-v", "--verbose", action="store_true",
         help="Print copied file paths to console.")
 
     args = parser.parse_args()
 
-    disperse_jsons(args.datadir, args.verbose)
+    disperse_jsons(args.target_dir, args.verbose)
 
     
