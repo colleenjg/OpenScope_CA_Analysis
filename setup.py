898bd4548531a0a7df1e0854bb619646a88e0ed9
Colleen J. Gillon <26847334+colleenjg@users.noreply.github.com>
2024-06-29 21:55:07 +0100

    Added setup.py and amended requirements.txt.
    minor

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = list()
+    for req in open("requirements.txt").readlines():
+        req = req.strip()
+        if "git+" in req and "util" in req:
+            req = f"util-colleenjg @ {req}"
+        requirements.append(req)
+
+setuptools.setup(
+    name="credassign",
+    version="0.0.1",
+    author="Colleen J. Gillon",
+    author_email="colleen.gillon@utoronto.ca",
+    description="Package for analysis of OpenScope Credit Assignment data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/colleenjg/OpenScope_CA_Analysis",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
